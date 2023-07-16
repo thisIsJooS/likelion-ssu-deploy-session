d53eb46ecc68bc09bcef8f13abbde114eb70bbcd
Seungwoo, Joo <thisis.joos@gmail.com>
2023-07-16 13:51:34 +0900

    feat: init setting

---
+++ src/main/java/ssu/likelion/deploysession/DeploySessionApplication.java
@@ -0,0 +1,13 @@
+package ssu.likelion.deploysession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploySessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploySessionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssu/likelion/deploysession/DeploySessionApplicationTests.java
@@ -0,0 +1,13 @@
+package ssu.likelion.deploysession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploySessionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
